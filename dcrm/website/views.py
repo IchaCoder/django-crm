548de2b60ea9ad21a6a8548d12d34070b4410f1d
IchaCoder <ichabordyeboah247@gmail.com>
2026-06-05 18:51:38 +0000

    Add record management features including forms and views
    - Implemented AddRecordForm for adding new records.
    - Created views for adding, updating, and deleting records.
    - Updated URLs to include paths for adding, updating, and deleting records.
    - Enhanced record display template with additional fields and action buttons.
    - Added templates for adding and updating records.
    - Updated navbar to include link for adding records.

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 
 from .models import Record
 
-from .forms import SignUpForm
+from .forms import AddRecordForm, SignUpForm
 
 # Create your views here.
 def home(request):
@@ -52,3 +52,41 @@ def customer_record(request, pk):
     else:
         messages.error(request, 'You must be logged in to view this page.')
         return redirect('home')
+    
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        customer_record.delete()
+        messages.success(request, 'Customer record has been deleted successfully.')
+        return redirect('home')
+    else:
+        messages.error(request, 'You must be logged in to perform this action.')
+        return redirect('home')
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
+
+
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
